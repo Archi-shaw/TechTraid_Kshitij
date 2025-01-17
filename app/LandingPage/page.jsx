fef6559589c2374024425f376bac10be76e90809
Archi Shaw <archishaw2004@gmail.com>
2025-01-18 03:24:51 +0530

    landing page added

--- app/LandingPage/page.jsx
+++ app/LandingPage/page.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
 
-const page = () => {
+const Page = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className='relative'>
+      <div className="background-image absolute inset-0 " >
+        <Image
+          src="/assets/landingpage.png" 
+          alt="Landing Page"
+          width={2000}
+          height={2000}
+          className='w-full max-h-screen'
+        />
+      </div>
+      <div className='relative z-10'>
+        <div className='navbar justify-between flex  w-full 
+        md:h-16 h-16 bg-white bg-opacity-40'>
+          <div className='logo px-3'>
+         <Link href='/'> 
+         <Image
+          src="/assets/workwiselogo.png" 
+          alt="Landing Page"
+          width={1600}
+          height={1600}
+          className='md:w-36 w-32 md:h-16 h-14 opacity-100 '
+        /></Link>
+          </div>
+          <div className='signup flex flex-row md:gap-7 gap-4 md:px-10 px-3 justify-center items-center'>
+            <Link href="/login">
+             <button className='login md:w-24 w-16 h-9 bg-[#FFF1B2] rounded-lg md:text-lg
+              text-sm font-semibold font-sans
+              text-black text-center'>
+               Login
+             </button></Link>
+             <Link href="/register">
+             <button className='login md:w-24 w-20 h-9 bg-[#FFF1B2] rounded-lg md:text-lg text-sm
+             font-semibold font-sans text-black text-center'>
+                Register
+             </button></Link>
+          </div>
+        </div>
+        <div className='body relative top-10'>
+          <div className='text'>
 
-export default page
+          </div>
+          <div className='box w-1/2 h-4/5 bg-black bg-opacity-40'>
+
+          </div>
+        </div>
+      </div></div>
+    </div>
+  );
+};
+
+export default Page;
